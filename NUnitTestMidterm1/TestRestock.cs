efc8d7784eab97a717301f4bc2bf13ca8f7f5c0d
ldasilva <ldasilva@eecs.wsu.edu>
2021-02-19 16:58:10 -0800

    also added test files

---
+++ NUnitTestMidterm1/TestRestock.cs
@@ -0,0 +1,28 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TestRestock.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Midterm1App
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Testing restock class
+    /// </summary>
+    public class TestRestock
+    {
+        /// <summary>
+        /// Test if FilterList() returns hash set with only distinct integers
+        /// Do not alter the list in any way 
+        /// Determine time complexity
+        /// Use MSDN to help with this
+        /// </summary>
+        [Test]
+        public void TestFilterList()
+        {
+
+        }
+
+    }
+}
---
+++ NUnitTestMidterm1/TestSearch.cs
@@ -0,0 +1,28 @@
+//-----------------------------------------------------------------------
+// <copyright file="TestSearch.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Midterm1App
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Testing search class
+    /// </summary>
+    public class TestSearch
+    {
+        /// <summary>
+        /// Test if FilterList() returns hash set with only distinct integers
+        /// Do not alter the list in any way 
+        /// Determine time complexity
+        /// Use MSDN to help with this
+        /// </summary>
+        [Test]
+        public void TestFilterList()
+        {
+
+        }
+
+    }
+}
