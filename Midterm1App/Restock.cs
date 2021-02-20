2307322dc81304a02ad91181eb896b3ee7436928
ldasilva <ldasilva@eecs.wsu.edu>
2021-02-19 19:48:40 -0800

    added test for Restock

--- Midterm1App/Restock.cs
+++ Midterm1App/Restock.cs
@@ -3,13 +3,39 @@
 //     Company copyright tag.
 // </copyright>
 //-----------------------------------------------------------------------
+using System.Collections.Generic;
+
 namespace Midterm1App
 {
     /// <summary>
-    /// Where employee will be asked if they wish to restock all products with less than N Items
+    /// all physical products with less than N items will be restocked, where N is provided by the employee
+    /// First, the list of products that match the search will be shown
+    /// Then, the employee will be asked whether she/he would like to restock for all of them
+    /// if yes then one by one, for products needed to restock, ask the the employee
+    /// how many purchases of that product should be made.
+    /// After that, a confirmation must be shown that the purchase is successful, and the updated product information must be shown.
+    /// if the user does not which to restock, the employee is given the option of new N or to return to main menu
     /// </summary>
     public class Restock
     {
+        /// <summary>
+        /// First, the list of products that match the search will be shown
+        /// Then, the employee will be asked whether she/he would like to restock for all of them
+        /// if yes then one by one, for products needed to restock, ask the the employee
+        /// how many purchases of that product should be made.
+        /// After that, a confirmation must be shown that the purchase is successful, and the updated product information must be shown.
+        /// if the user does not which to restock, the employee is given the option of new N or to return to main menu
+        /// </summary>
+        /// <param name="pList">List of products Matching search</param>
+        /// <param name="n">Quantity n</param>
+        public List<Product> CheckStock(List<Product> pList, int n)
+        {
+            return pList;
+        }
 
+        public List<Product> RestockProducts(List<Product> lowInventoryItems)
+        {
+            return lowInventoryItems;
+        }
     }
 }
