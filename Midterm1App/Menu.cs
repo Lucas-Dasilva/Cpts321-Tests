0bfe09ea41d8c76897c3e2bec2eb6bf7815752db
ldasilva <ldasilva@eecs.wsu.edu>
2021-02-20 17:10:03 -0800

    Added menu prototype

---
+++ Midterm1App/Menu.cs
@@ -0,0 +1,152 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Menu.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Midterm1App
+{
+    using System;
+
+    /// <summary>
+    /// A custom console menu made by Lucas Da Silva
+    /// Go Cougs!
+    /// </summary>
+    public class Menu
+    {
+        /// <summary>
+        /// Points to current option
+        /// </summary>
+        private int selectedIndex;
+
+        /// <summary>
+        /// Array of menu options
+        /// </summary>
+        private string[] options;
+
+        /// <summary>
+        /// Start up message 
+        /// </summary>
+        private string prompt;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Menu"/> class.
+        /// </summary>
+        /// <param name="prompt">Main menu message</param>
+        /// <param name="options">Array of menu options</param>
+        public Menu(string prompt, string[] options)
+        {
+            this.prompt = prompt;
+            this.options = options;
+            this.selectedIndex = 0;
+        }
+
+        /// <summary>
+        /// This will handle all the arrow key inputs
+        /// </summary>
+        /// <returns>Get back selected index</returns>
+        public int Run()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                // Set standard background because string.format bug
+                Console.ForegroundColor = System.ConsoleColor.White;
+                Console.BackgroundColor = System.ConsoleColor.Black;
+                Console.Clear(); // Clear console
+                this.DisplayOptions(); // Redraw new Options
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true); // Get key pressed
+                keyPressed = keyInfo.Key;
+
+                // if keypressed is up arrow lower index
+                // if key is down arrow increase index
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    this.selectedIndex--;
+
+                    // if index is at the top edge, change index to lowest option
+                    if (this.selectedIndex == -1)
+                    {
+                        this.selectedIndex = this.options.Length - 1;
+                    }
+                }
+
+                if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    this.selectedIndex++;
+
+                    // if index is at lowest edge, change index to highest option
+                    if (this.selectedIndex == this.options.Length)
+                    {
+                        this.selectedIndex = 0;
+                    }
+                }
+            } while (keyPressed != ConsoleKey.Enter);
+
+            return this.selectedIndex;
+        }
+
+        /// <summary>
+        /// Run option 1
+        /// </summary>
+        public void FirstOption()
+        {
+            // Set standard background because string.format bug
+            Console.ForegroundColor = System.ConsoleColor.White;
+            Console.BackgroundColor = System.ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Option 1");
+            Console.WriteLine("\nPress any key to go back to menu...");
+            Console.ReadKey(true);
+        }
+
+        /// <summary>
+        /// Run option 2
+        /// </summary>
+        public void SecondOption()
+        {
+            // Set standard background because string.format bug
+            Console.ForegroundColor = System.ConsoleColor.White;
+            Console.BackgroundColor = System.ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Option 2");
+            Console.WriteLine("\nPress any key to go back to menu...");
+            Console.ReadKey(true);
+        }
+
+        /// <summary>
+        /// Display the options to console window
+        /// </summary>
+        private void DisplayOptions()
+        {
+            // Main menu message
+            Console.WriteLine(this.prompt);
+
+            // List out options
+            for (int i = 0; i < this.options.Length; i++)
+            {
+                string currentOption = this.options[i];
+                string prefix; // Points to the option selected
+
+                // if selected index points to the option, then highlight it
+                // else don't highlight it 
+                if (i == this.selectedIndex)
+                {
+                    prefix = ">>";
+                    Console.ForegroundColor = System.ConsoleColor.White;
+                    Console.BackgroundColor = System.ConsoleColor.DarkRed;
+                }
+                else
+                {
+                    prefix = "  ";
+                    Console.ForegroundColor = System.ConsoleColor.White;
+                    Console.BackgroundColor = System.ConsoleColor.Black;
+                }
+
+                // Format option and write to console
+                string menuStr = string.Format("{0,-2} {1,-12}", prefix, currentOption);
+                Console.WriteLine(menuStr);
+            }
+        }
+    }
+}
---
+++ Midterm1App/Setup.cs
@@ -0,0 +1,78 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Menu.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Midterm1App
+{
+    using System.Collections.Generic;
+    using System;
+    using Newtonsoft.Json;
+
+    class Setup
+    {
+        public void Start()
+        {
+            // Menu options
+            string prompt = "(Use arrow keys to cycle through options and press enter to select it)";
+            string[] options = { "Start", "Settings", "Exit" };
+
+            // Initialize objects
+            Menu mainMenu = new Menu(prompt, options);
+            Search search = new Search();
+            Restock restock = new Restock();
+            List<Product> productList =  GenerateProducts(new List<Product>());
+
+            Console.Write("Enter Search Input: ");
+            string input = Console.ReadLine();
+            List<Product> searchResult = new List<Product>(search.MatchSearch(productList, input));
+            search.PrintSimpleResults(searchResult);
+
+
+            Console.WriteLine("(1)Search\n(2)Save Search\n(3)Restock");
+
+            search.PrintFullResults(searchResult);
+            string output = JsonConvert.SerializeObject(productList, Formatting.Indented);
+            // save the search
+            int selectedIndex = mainMenu.Run();
+            do
+            {
+                switch (selectedIndex)
+                {
+                    case 0:
+                        // Run first option
+                        mainMenu.FirstOption();
+                        break;
+                    case 1:
+                        // Run second option
+                        mainMenu.SecondOption();
+                        break;
+                    case 2:
+                        // Run third option
+                        break;
+                }
+
+                selectedIndex = mainMenu.Run();
+            } while (selectedIndex != 2);
+        }
+        private List<Product> GenerateProducts(List<Product> productList)
+        {
+            productList.Add(new Product(1, "Playstation 5", 0, "The brand new console PS5, where you can play games"));
+            productList.Add(new Product(2, "PlayBoy Mag", 2, "Newest playboy magazine with Cardi B"));
+            productList.Add(new Product(3, "Control", 10, "Play the Control on new generation of consoles"));
+            productList.Add(new Product(4, "Door Mirror", 2, "A mirror designed to be hangned on your wall"));
+            productList.Add(new Product(5, "$50 PSN Card", -1, "Giftcard for for PSN store"));
+            productList.Add(new Product(6, "Skii Boots", 5, "Control your lines with extreme precision with these highgrade boots used for skiing, for adults"));
+            productList.Add(new Product(7, "Windows 10", -1, "The #1 OS that everyone uses, and is also way better than Linux"));
+            productList.Add(new Product(8, "Pen", 1232, "The best pens in the world, jk they're cheap, that's why we have so many of them"));
+            productList.Add(new Product(9, "Iphone 10", 4, "The best phone you can get right now, play games and movies in stunning 360P quality"));
+            productList.Add(new Product(10, "PS3 Controller", 3, "A controller used to control games on the playstation 3"));
+
+            return productList;
+
+        }
+
+
+    }
+}
